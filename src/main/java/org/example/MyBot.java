adf02445b3bee7b1255aae9282d604db660c71c2
Abror <abrorbekburxonov2010@gmail.com>
2025-11-21 14:54:39 +0500

    add commit

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -1,22 +1,31 @@
 package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageMedia;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.media.InputMediaPhoto;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 
 public class MyBot extends TelegramLongPollingBot {
 
     MyBotService myBotService = new MyBotService();
+    int phtoNum = 13;
 
     @Override
     public void onUpdateReceived(Update update) {
 
 
-        if(update.getMessage().hasText() && update.hasMessage()) {
+
+        if(update.hasMessage() && update.getMessage().hasText()) {
 
             Long chatId = update.getMessage().getChatId();
             String text = update.getMessage().getText();
+
+
 
             if(text.equals("/start")){
                 try {
@@ -30,15 +39,188 @@ public class MyBot extends TelegramLongPollingBot {
         else if (update.hasCallbackQuery()) {
             CallbackQuery callbackQuery = update.getCallbackQuery();
             String data = callbackQuery.getData();
-            long ChatId = callbackQuery.getMessage().getChatId();
+            Long chatId = callbackQuery.getMessage().getChatId();
             Integer messageId = callbackQuery.getMessage().getMessageId();
             System.out.println(data);
 
-            if(data.equals("/stop")){
+            if(data.equals("InternetdanModelTopishId")){
+                EditMessageText editMessageText = new EditMessageText();
+                editMessageText.setChatId(chatId);
+                editMessageText.setMessageId(messageId);
+                editMessageText.setText(" shu saytlardan model topishingiz mumkun:\n" +
+                        "                https://www.printables.com/\n" +
+                        "                https://www.thingiverse.com/");
+
+                editMessageText.setReplyMarkup(myBotService.OrtgaQaeytargich(chatId));
+
+                try {
+                    execute(editMessageText);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if(data.equals("OrtgaId")){
+                EditMessageText editMessageText = new EditMessageText();
+                editMessageText.setChatId(chatId);
+                editMessageText.setMessageId(messageId);
+                editMessageText.setText(" Assalomu alaykum!\n" +
+                        "tanlangan faylini @AbrorbekBurxonov159 ga jonating\uD83E\uDDF1;\n" +
+                        "⚙\uFE0F3d Printerda narsalar chiqarishni Boshlang⚙\uFE0F");
+
+                editMessageText.setReplyMarkup(myBotService.StarterInline(chatId));
+
+                try {
+                    execute(editMessageText);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if(data.equals("OldingiSahifaId")){
+                DeleteMessage  deleteMessage = new DeleteMessage();
+                deleteMessage.setChatId(chatId);
+                deleteMessage.setMessageId(messageId);
+
+                try {
+                    execute(deleteMessage);
+                    execute(myBotService.Start(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if(data.equals("narxlarId")){
+                EditMessageText editMessageText = new EditMessageText();
+                editMessageText.setChatId(chatId);
+                editMessageText.setMessageId(messageId);
+                editMessageText.setText("""
+                        Narxlanish quydagicha boladi :
+                        PLA oddiy rangli filoment(plastik) [1gramm - 700 sum]: oddiy holatda ishlatilaydigan modellar , oyinchoqlar uchun togri keladi \n 
+                        TPU filoment(plastik) [1gramm - 800 sum]: yumshoq , rezina xossalariga ega bolgan plastik , qattiq bolmagam jismlar uchun ishlatilinadi \n
+                        ABS filomet(plastik) [1gramm - 900 sum]:PLA ga qaraganda mustqakamroq plastik turi
+                        """);
+                editMessageText.setReplyMarkup(myBotService.OrtgaQaeytargich(chatId));
+                try {
+                    execute(editMessageText);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
 
             }
 
+            if(data.equals("chiqarishShartlariId")){
+                EditMessageText editMessageText = new EditMessageText();
+                editMessageText.setChatId(chatId);
+                editMessageText.setMessageId(messageId);
+                editMessageText.setText("""
+                        Printer platformasininig razmerlari 220mm X 220mm tashkil etadi;\n
+                        Printer balandligi 270mm tashkil etadi;\n
+                        tanlangan modellar shu razmerlarga mos kelish kerak , agar tolliq model kerak bolsa modelni bolib chiqarish hizmati bor
+                        """);
 
+                editMessageText.setReplyMarkup(myBotService.ShartlarInline(chatId));
+
+                try {
+                    execute(editMessageText);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if(data.equals("AloqaId")) {
+                EditMessageText editMessageText = new EditMessageText();
+                editMessageText.setChatId(chatId);
+                editMessageText.setMessageId(messageId);
+                editMessageText.setText("Boglanish uchun: @AbrorbekBurxonov159");
+                editMessageText.setReplyMarkup(myBotService.OrtgaQaeytargich(chatId));
+
+                try {
+                    execute(editMessageText);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+
+            if(data.equals("ChiqarilganProektalarId")) {
+
+                EditMessageMedia editMessageMedia = new EditMessageMedia();
+                editMessageMedia.setChatId(chatId);
+                editMessageMedia.setMessageId(messageId);
+                editMessageMedia.setMedia(new InputMediaPhoto("https://t.me/ImageBaze/" + phtoNum + ""));
+
+                editMessageMedia.setReplyMarkup(myBotService.PhotoInline());
+
+                try {
+                    execute(editMessageMedia);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if(data.equals("KeyingiId")){
+                if(phtoNum < 20){
+                    phtoNum = phtoNum+1;
+                    EditMessageMedia editMessageMedia = new EditMessageMedia();
+                    editMessageMedia.setChatId(chatId);
+                    editMessageMedia.setMessageId(messageId);
+                    editMessageMedia.setMedia(new InputMediaPhoto("https://t.me/ImageBaze/" + phtoNum + ""));
+                    editMessageMedia.setReplyMarkup(myBotService.PhotoInline());
+                    try {
+                        execute(editMessageMedia);
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println(phtoNum);
+
+
+            }
+            if(data.equals("OldingiId")){
+                if(phtoNum > 13){
+                    phtoNum = phtoNum-1;
+                    EditMessageMedia editMessageMedia = new EditMessageMedia();
+                    editMessageMedia.setChatId(chatId);
+                    editMessageMedia.setMessageId(messageId);
+                    editMessageMedia.setMedia(new InputMediaPhoto("https://t.me/ImageBaze/" + phtoNum + ""));
+                    editMessageMedia.setReplyMarkup(myBotService.PhotoInline());
+                    try {
+                        execute(editMessageMedia);
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println(phtoNum);
+            }
+
+
+            if(data.equals("ZakazgaModelId")) {
+                EditMessageText editMessageText = new EditMessageText();
+                editMessageText.setChatId(chatId);
+                editMessageText.setMessageId(messageId);
+                editMessageText.setText("\uD83D\uDCD0 Zakazga model yasash\n" +
+                        "\n" +
+                        "Bu bo‘limda siz chizmalaringiz asosida 3D model buyurtma qilishingiz mumkin.\n" +
+                        "Iltimos, quyidagilarni yodda tuting:\n" +
+                        "\n" +
+                        "Model juda murakkab bo‘lmasligi kerak.\n" +
+                        "\n" +
+                        "Siz taqdim qilgan chizma yoki rasm asosida model tayyorlanadi.\n" +
+                        "\n" +
+                        "tayyor modelni 3D bosib berilgan holatda, chop etilgan buyum narxiga +30% xizmat haqi qo‘shiladi.\n" +
+                        "\n" +
+                        "Buyurtma berish uchun model haqida qisqacha ma’lumot va chizmalarni yuboring.");
+                editMessageText.setReplyMarkup(myBotService.OrtgaQaeytargich(chatId));
+
+
+
+                try {
+                    execute(editMessageText);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
 
 
--- src/main/java/org/example/MyBotService.java
+++ src/main/java/org/example/MyBotService.java
@@ -2,21 +2,19 @@ package org.example;
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MyBotService  {
 
-    public SendMessage Start(long chatId){
+    public SendMessage Start(Long chatId){
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
         sendMessage.setText("""
                 Assalomu alaykum!
+                Chiqarmoqchi bolgan modelingizni faylini jonating👾;
                 ⚙️3d Printerda narsalar chiqarishni Boshlang⚙️
                 """);
 
@@ -43,14 +41,133 @@ public class MyBotService  {
 
         List<InlineKeyboardButton> keyboardRow3 = new ArrayList<>();
         InlineKeyboardButton button4 = new InlineKeyboardButton();
-        button4.setText("Chiqarilgan Proektlar \uD83D\uDEE0");
-        button4.setCallbackData("ChiqarilganProektlarId");
+        button4.setText("Zakazga model yasash\uD83D\uDEE0");
+        button4.setCallbackData("ZakazgaModelId");
         keyboardRow3.add(button4);
         keyboardRows.add(keyboardRow3);
+
+        List<InlineKeyboardButton> keyboardRow4 = new ArrayList<>();
+        InlineKeyboardButton button5 = new InlineKeyboardButton();
+        button5.setText("Aloqaga chiqish");
+        button5.setCallbackData("AloqaId");
+        keyboardRow4.add(button5);
+        InlineKeyboardButton button6 = new InlineKeyboardButton();
+        button6.setText("Chiqarilgan Proektalar");
+        button6.setCallbackData("ChiqarilganProektalarId");
+        keyboardRow4.add(button6);
+        keyboardRows.add(keyboardRow4);
 
         inlineKeyboardMarkup.setKeyboard(keyboardRows);
         sendMessage.setReplyMarkup(inlineKeyboardMarkup);
         return sendMessage;
     }
+
+    public InlineKeyboardMarkup StarterInline(Long chatId){
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboardRows = new ArrayList<>();
+
+        List<InlineKeyboardButton> keyboardRow = new ArrayList<>();
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setText("Internetdan Model Topish \uD83D\uDCBB");
+        button1.setCallbackData("InternetdanModelTopishId");
+        keyboardRow.add(button1);
+        keyboardRows.add(keyboardRow);
+
+        List<InlineKeyboardButton> keyboardRow2 = new ArrayList<>();
+        InlineKeyboardButton button2 = new InlineKeyboardButton();
+        button2.setText("narxlar \uD83D\uDCB0");
+        button2.setCallbackData("narxlarId");
+        keyboardRow2.add(button2);
+        InlineKeyboardButton button3 = new InlineKeyboardButton();
+        button3.setText("chiqarish shartlari \uD83D\uDD79");
+        button3.setCallbackData("chiqarishShartlariId");
+        keyboardRow2.add(button3);
+        keyboardRows.add(keyboardRow2);
+
+        List<InlineKeyboardButton> keyboardRow3 = new ArrayList<>();
+        InlineKeyboardButton button4 = new InlineKeyboardButton();
+        button4.setText("Zakazga model yasash\uD83D\uDEE0");
+        button4.setCallbackData("ZakazgaModelId");
+        keyboardRow3.add(button4);
+        keyboardRows.add(keyboardRow3);
+
+        List<InlineKeyboardButton> keyboardRow4 = new ArrayList<>();
+        InlineKeyboardButton button5 = new InlineKeyboardButton();
+        button5.setText("Aloqaga chiqish");
+        button5.setCallbackData("AloqaId");
+        keyboardRow4.add(button5);
+        InlineKeyboardButton button6 = new InlineKeyboardButton();
+        button6.setText("Chiqarilgan Proektalar");
+        button6.setCallbackData("ChiqarilganProektalarId");
+        keyboardRow4.add(button6);
+        keyboardRows.add(keyboardRow4);
+
+        inlineKeyboardMarkup.setKeyboard(keyboardRows);
+
+        return inlineKeyboardMarkup;
+    }
+
+    public InlineKeyboardMarkup OrtgaQaeytargich(Long chatId){
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboardRows = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardRow = new ArrayList<>();
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setText("⬅\uFE0FOrtga");
+        button1.setCallbackData("OrtgaId");
+        keyboardRow.add(button1);
+        keyboardRows.add(keyboardRow);
+
+
+        inlineKeyboardMarkup.setKeyboard(keyboardRows);
+
+        return inlineKeyboardMarkup;
+    }
+
+    public InlineKeyboardMarkup ShartlarInline(Long chatId){
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboardRows = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardRow = new ArrayList<>();
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setText("⬅\uFE0FOrtga");
+        button1.setCallbackData("OrtgaId");
+        keyboardRow.add(button1);
+        keyboardRows.add(keyboardRow);
+        inlineKeyboardMarkup.setKeyboard(keyboardRows);
+
+        return inlineKeyboardMarkup;
+    }
+
+    public  InlineKeyboardMarkup PhotoInline(){
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboardRows = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardRow = new ArrayList<>();
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setText("Keyingi➡\uFE0F");
+        button1.setCallbackData("KeyingiId");
+        InlineKeyboardButton button2 = new InlineKeyboardButton();
+        button2.setText("⬅\uFE0FOldingi");
+        button2.setCallbackData("OldingiId");
+        List<InlineKeyboardButton> keyboardRow1 = new ArrayList<>();
+        InlineKeyboardButton button3 = new InlineKeyboardButton();
+        button3.setText("⬅\uFE0FOldingi sahifaga qaytish");
+        button3.setCallbackData("OldingiSahifaId");
+        keyboardRow1.add(button3);
+        keyboardRows.add(keyboardRow1);
+
+        keyboardRow.add(button2);
+        keyboardRow.add(button1);
+
+
+        keyboardRows.add(keyboardRow);
+        inlineKeyboardMarkup.setKeyboard(keyboardRows);
+
+        return inlineKeyboardMarkup;
+    }
+
+
+
+
+
 
 }
